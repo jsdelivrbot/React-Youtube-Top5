f7fe59e5e4c71ea4a654d876cea81eb9ed0b75a8
Xiaokai Wang <xiw69@pitt.edu>
2017-02-11 01:17:11 -0500

    export SearchBar

--- ReduxSimpleStarter/src/index.js
+++ ReduxSimpleStarter/src/index.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// write the path for our own created files
+import SearchBar from './components/search_bar';
+
+
+const API_KEY = 'AIzaSyAt2rKKfigSTRp2JgE2rzFifiURCqCfBc8'; // youtube api key in string
+
 // Create a new component, should produce some HTML
 // declare a function with new ES 6 syntax
 const App = () => {
-    return <div>Hi!</div>; //jsx cannot be interpreted by the browser
-}
+    return (
+        <div>
+            <SearchBar />
+        </div>
+    ); //jsx cannot be interpreted by the browser
+};
 
 
 // Take this component's generated HTML and put it on the page(in the DOM)
