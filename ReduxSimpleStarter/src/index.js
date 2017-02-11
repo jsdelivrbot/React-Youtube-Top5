932e01b45a1aa10921643cbe4005ae8bf7cfbf97
Xiaokai Wang <xiw69@pitt.edu>
2017-02-11 00:37:11 -0500

    Naive try of showing to the DOM

--- ReduxSimpleStarter/src/index.js
+++ ReduxSimpleStarter/src/index.js
@@ -1,10 +1,13 @@
-// Create a new component, should produce some HTML
+import React from 'react';
 
+// Create a new component, should produce some HTML
 const App = function(){
-    return <div>Hi!</div>;
+    return <div>Hi!</div>; //jsx cannot be interpreted by the browser
 }
 
 
 // Take this component's generated HTML and put it on the page(in the DOM)
+React.render(App);
+
 
 //
