4f46f2cc608b8a02d323fb0d5b877a12ef41101b
Xiaokai Wang <xiw69@pitt.edu>
2017-02-15 02:33:48 -0500

    display search items on DOM

--- ReduxSimpleStarter/src/components/video_list.js
+++ ReduxSimpleStarter/src/components/video_list.js
@@ -2,10 +2,17 @@ import React from 'react';
 import VideoListItem from './video_list_item';
 
 const VideoList = (props) => {
+    // use map instead of for loop in javascript to avoid typo
+    const videoItems = props.videos.map((video) => {
+        // add id to our each piece of data
+        return <VideoListItem key = {video.etag} video = {video} />
+    });
+
+
     return (
         // className --> bootstrap syntax
         <ul className = "col-md-4 list-group">
-            {props.videos.length}
+            {videoItems}
         </ul>
     );
 };
